6c5379984d5ab60f4a6c6a7fe2c56f7023f42751
Leander Diaz-Bone <leanderdiazbone@gmail.com>
2023-07-17 17:48:53 +0200

    Fixed issue with metrics_epochs experiment.

--- test.py
+++ test.py
@@ -1,15 +1,12 @@
 import numpy as np
 import scanpy as sc
-import anndata
 import scvi
-from scib_metrics.benchmark import Benchmarker
-import torch
 from functions import *
-import scib_metrics
 from lightning.pytorch.callbacks import ModelCheckpoint
+import anndata
 
-adata = anndata.read_h5ad(filename="data/adataLung4.h5ad")
+adata = anndata.read_h5ad(filename="/local/data/BachelorThesis/data/adataLung4.h5ad")
 scvi.model.SCVI.setup_anndata(adata, layer="counts")
 vaeSD = scvi.model.SCVI(adata)
-checkpoint_callback = ModelCheckpoint(dirpath= "models/test/",save_last=True,filename="{epoch:02d}",every_n_epochs=10)
-vaeSD.train(default_root_dir="models/test/",enable_checkpointing = True,  max_epochs = 20, callbacks=[checkpoint_callback])
+checkpoint_callback = ModelCheckpoint(dirpath= "/local/data/BachelorThesis/models/test/",save_last=False,filename="{epoch:d}",every_n_epochs=10,save_top_k=-1)
+vaeSD.train(enable_checkpointing = True,  max_epochs = 100, callbacks=[checkpoint_callback], check_val_every_n_epoch=1)
